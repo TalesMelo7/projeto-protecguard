2fd9f8ef1df2adfa33f2f369159206194ba828e7
Tales Melo <tales-melo@hotmail.com>
2025-05-26 21:01:14 -0300

    Estilização da página recuperar-senha.html

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 function entrar() {
+    if(usuario != usuario || senha != senha){
+        alert("Usuário ou senha inválido")
+    } else {
+        alert("Login efetuado com sucesso!")
+    }
+}
+
+
+function enviar() {
     
 }
