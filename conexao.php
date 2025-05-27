d112b5761db78642a79497b4445f8ff495d25ae8
Lucas costa <lucasproksggb@gmail.com>
2025-05-26 23:56:51 -0300

    Desenvolvimento BASICO do conexao e index PHP

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php
+$hostname = "localhost";
+$bancodedados = "clientes";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+if ($mysqli->connect_errno){
+    echo "falha ao conectar:(" . $mysqli->connect_errno . ")" . $mysqli->connect_errno;
+
+}
+else
+echo "Conectando ao Banco de Dados";
+
+?>
