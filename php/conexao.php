ad4fc088cfae7e97b7b037b2d721f4a6da43f71f
Tales Melo <tales-melo@hotmail.com>
2025-05-30 17:23:47 -0300

    Mexi em tudo e mais um pouco, mas principalmente na conexao.php

---
+++ php/conexao.php
@@ -0,0 +1,10 @@
+<?php
+// TESTE DEFINITIVO PARA VER SE ESTE ARQUIVO ESTÁ SENDO EXECUTADO PELO SERVIDOR
+
+$mensagemUnica = "CONFIRMADO! O ARQUIVO EM C:\\xampp\\htdocs\\protecguard\\php\\conexao.php FOI CARREGADO PELO NAVEGADOR. Data e Hora: " . date("Y-m-d H:i:s");
+
+die($mensagemUnica); // A função die() para o script aqui e mostra a mensagem.
+
+// NADA ABAIXO DESTA LINHA SERÁ EXECUTADO NESTE TESTE.
+// O código original da conexão está comentado ou removido para este teste.
+?>
